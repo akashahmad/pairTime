fc056ca2740c6cc86387213a35656d52438c39ee
akashahmad <engr.akashahmad@gmail.com>
2022-02-22 16:32:57 +0500

    update navbar styling on homepage

--- utils/js/dashboard.js
+++ utils/js/dashboard.js
@@ -2,10 +2,15 @@ const dashboardOpenBtn = document.getElementById("dashb-nav-openBtn");
 const dashboardCloseBtn = document.getElementById("dashb-nav-closeBtn");
 const sidebar = document.getElementById("sidebar");
 
-dashboardOpenBtn.addEventListener("click", () => {
+console.log(dashboardOpenBtn);
+
+dashboardOpenBtn.addEventListener("click", (e) => {
+  console.log("clicked", e);
   sidebar.style.left = "0";
 });
 
 dashboardCloseBtn.addEventListener("click", () => {
   sidebar.style.left = "-100%";
 });
+
+console.log("dashboard");
