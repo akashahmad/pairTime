38de3770ad2934c5d7308a97a87388495347c2ac
akashahmad <engr.akashahmad@gmail.com>
2022-06-15 00:20:38 +0500

    created post content page

--- utils/js/calendar.js
+++ utils/js/calendar.js
@@ -7,7 +7,7 @@
 
 /////////////////////////////////////// EVENT LISTENERS //////////////////////////////////////////
 // dateBtn.addEventListener(`click`, addEvent);
-document.addEventListener("DOMContentLoaded", calendarLoad);
+// document.addEventListener("DOMContentLoaded", calendarLoad);
 // datePopup.addEventListener(`click`, (e) => {
 //   if (e.target === datePopup) {
 //     datePopup.classList.add(`pointer-events-none`, `opacity-0`);
@@ -33,69 +33,69 @@ document.addEventListener("DOMContentLoaded", calendarLoad);
 //   calendarLoad();
 // }
 
-let currentDate = new Date().getDate();
+// let currentDate = new Date().getDate();
 
-const calendarEvents = [
-  {
-    start: `2022-05-${
-      currentDate < 10 ? `0` + currentDate : currentDate
-    }T03:30:00`,
-    end: `2022-05-${
-      currentDate < 10 ? `0` + currentDate : currentDate
-    }T04:30:00`,
-  },
-  {
-    start: `2022-05-${
-      currentDate + 2 < 10 ? `0` + (currentDate + 2) : currentDate + 2
-    }T07:30:00`,
-    end: `2022-05-${
-      currentDate + 2 < 10 ? `0` + (currentDate + 2) : currentDate + 2
-    }T09:15:00`,
-  },
-  {
-    start: `2022-05-${
-      currentDate + 1 < 10 ? `0` + (currentDate + 1) : currentDate + 1
-    }T11:00:00`,
-    end: `2022-05-${
-      currentDate + 1 < 10 ? `0` + (currentDate + 1) : currentDate + 1
-    }T12:30:00`,
-  },
-];
+// const calendarEvents = [
+//   {
+//     start: `2022-05-${
+//       currentDate < 10 ? `0` + currentDate : currentDate
+//     }T03:30:00`,
+//     end: `2022-05-${
+//       currentDate < 10 ? `0` + currentDate : currentDate
+//     }T04:30:00`,
+//   },
+//   {
+//     start: `2022-05-${
+//       currentDate + 2 < 10 ? `0` + (currentDate + 2) : currentDate + 2
+//     }T07:30:00`,
+//     end: `2022-05-${
+//       currentDate + 2 < 10 ? `0` + (currentDate + 2) : currentDate + 2
+//     }T09:15:00`,
+//   },
+//   {
+//     start: `2022-05-${
+//       currentDate + 1 < 10 ? `0` + (currentDate + 1) : currentDate + 1
+//     }T11:00:00`,
+//     end: `2022-05-${
+//       currentDate + 1 < 10 ? `0` + (currentDate + 1) : currentDate + 1
+//     }T12:30:00`,
+//   },
+// ];
 
-function calendarLoad() {
-  var calendarEl = document.getElementById("schedular-calendar");
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "timeGridWeek",
-    selectable: true,
-    editable: true,
-    droppable: true,
-    allDaySlot: false,
-    slotLabelFormat: {
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: false,
-      meridiem: false,
-    },
-    events: calendarEvents,
-    eventTimeFormat: {
-      hour: "numeric",
-      minute: "2-digit",
-      // hour12: false,
-      meridiem: "long",
-    },
-    // select: function (info) {
-    //   datePopup.classList.remove(`pointer-events-none`, `opacity-0`);
-    //   startDate = info.startStr.split(`T`)[0];
-    //   endDate = info.startStr.split(`T`)[0];
+// function calendarLoad() {
+//   var calendarEl = document.getElementById("schedular-calendar");
+//   var calendar = new FullCalendar.Calendar(calendarEl, {
+//     initialView: "timeGridWeek",
+//     selectable: true,
+//     editable: true,
+//     droppable: true,
+//     allDaySlot: false,
+//     slotLabelFormat: {
+//       hour: "2-digit",
+//       minute: "2-digit",
+//       hour12: false,
+//       meridiem: false,
+//     },
+//     events: calendarEvents,
+//     eventTimeFormat: {
+//       hour: "numeric",
+//       minute: "2-digit",
+//       // hour12: false,
+//       meridiem: "long",
+//     },
+// select: function (info) {
+//   datePopup.classList.remove(`pointer-events-none`, `opacity-0`);
+//   startDate = info.startStr.split(`T`)[0];
+//   endDate = info.startStr.split(`T`)[0];
 
-    //   dateStartInput.value = info.startStr.split(`+`)[0].split(`T`)[1];
-    //   dateEndInput.value = info.endStr.split(`+`)[0].split(`T`)[1];
-    // },
-    // eventDragStart: function (info) {
-    //   console.log(info);
-    //   console.log(info.view.getCurrentData().currentDate);
-    // },
-  });
+//   dateStartInput.value = info.startStr.split(`+`)[0].split(`T`)[1];
+//   dateEndInput.value = info.endStr.split(`+`)[0].split(`T`)[1];
+// },
+// eventDragStart: function (info) {
+//   console.log(info);
+//   console.log(info.view.getCurrentData().currentDate);
+// },
+//   });
 
-  calendar.render();
-}
+//   calendar.render();
+// }
